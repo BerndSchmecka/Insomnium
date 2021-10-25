3a7a4cd645ed1c81b99e66d2a3030cc27b2db377
Dunkelmann <bernd.schmecka@gmail.com>
2021-10-25 13:38:14 +0200

    Init Project

---
+++ Insomnium/src/Insomnium/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insomnium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
