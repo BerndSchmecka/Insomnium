22f3377db1bec1b343d6d1f80074a29625c41231
Dunkelmann <bernd.schmecka@gmail.com>
2021-10-25 21:48:51 +0200

    Improve Implementation

--- Insomnium/src/Insomnium/MatrixServer.cs
+++ Insomnium/src/Insomnium/MatrixServer.cs
@@ -7,18 +7,29 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Insomnium {
+
+    struct ResponseData {
+        public HttpListenerResponse Response {get;}
+        public byte[] Data {get;}
+        public string ContentType {get;}
+        public int StatusCode {get;}
+
+        public ResponseData(HttpListenerResponse resp, string data_to_send, string contentType, int statusCode){
+            this.Response = resp;
+            this.Data = Encoding.UTF8.GetBytes(data_to_send);
+            this.ContentType = contentType;
+            this.StatusCode = statusCode;
+
+            this.Response.ContentType = this.ContentType;
+            this.Response.ContentEncoding = Encoding.UTF8;
+            this.Response.ContentLength64 = this.Data.LongLength;
+            this.Response.StatusCode = this.StatusCode;
+        }
+    }
+
     class MatrixServer {
         private static HttpListener listener;
         private static string url = "http://*:8008/";
-
-        private static byte[] setResponseData(ref HttpListenerResponse resp, string data_to_send, string contentType, int statusCode) {
-            byte[] data = Encoding.UTF8.GetBytes(data_to_send);
-            resp.ContentType = contentType;
-            resp.ContentEncoding = Encoding.UTF8;
-            resp.ContentLength64 = data.LongLength;
-            resp.StatusCode = statusCode;
-            return data;
-        }
 
         private async Task HandleInboundConnections() {
             bool runServer = true;
@@ -29,15 +40,15 @@ namespace Insomnium {
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
 
-                byte[] data = setResponseData(ref response, "404 Not Found", "text/plain", 404);
+                ResponseData rd = new ResponseData(response, "404 Not Found", "text/plain", 404);
 
                 switch(request.Url.AbsolutePath) {
                     case "/_matrix/client/versions":
-                        data = setResponseData(ref response, JsonConvert.SerializeObject(new JSON_Versions(Program.SUPPORTED_CLIENT_VERSIONS)), "application/json", 200);
+                        rd = new ResponseData(response, JsonConvert.SerializeObject(new JSON_Versions(Program.SUPPORTED_CLIENT_VERSIONS)), "application/json", 200);
                         break;
                 }
                 
-                await response.OutputStream.WriteAsync(data, 0, data.Length);
+                await response.OutputStream.WriteAsync(rd.Data, 0, rd.Data.Length);
                 response.Close();
             }
         }
