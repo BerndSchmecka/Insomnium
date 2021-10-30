3915acbf1adfa61b0e7c41a197201ecf83d112a5
Dunkelmann <bernd.schmecka@gmail.com>
2021-10-30 12:53:41 +0200

    Add check for null

--- Insomnium/src/Insomnium/LoginHandler.cs
+++ Insomnium/src/Insomnium/LoginHandler.cs
@@ -8,7 +8,11 @@ namespace Insomnium {
 
          public LoginHandler(string requestStr, HttpListenerResponse response) {
              dynamic req = JsonConvert.DeserializeObject(requestStr);
-             LoginResponse = new ResponseData(response, JsonConvert.SerializeObject(new JSON_Login_Response("@" + req.user + ":dev.dunkelmann.eu", "sdsdasdsds", "dev.dunkelmann.eu", "ABCDEFGHI")), "application/json", 200);
+             if(req.user == null){
+                 LoginResponse = new ResponseData(response, "400 Bad request", "application/json", 400);
+             } else {
+                 LoginResponse = new ResponseData(response, JsonConvert.SerializeObject(new JSON_Login_Response("@" + req.user + ":dev.dunkelmann.eu", "sdsdasdsds", "dev.dunkelmann.eu", "ABCDEFGHI")), "application/json", 200);
+             }
          }
     }
 }
