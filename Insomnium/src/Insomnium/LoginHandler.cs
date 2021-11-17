8dbf0ad5fd6b8efef64769b7ec9c942ba1a80bb4
Dunkelmann <bernd.schmecka@gmail.com>
2021-11-17 11:46:13 +0100

    Changes To Login + Main Program

--- Insomnium/src/Insomnium/LoginHandler.cs
+++ Insomnium/src/Insomnium/LoginHandler.cs
@@ -1,18 +1,57 @@
 using Newtonsoft.Json;
 using System.Net;
+using System;
 
 namespace Insomnium {
+
+    enum ELoginErrorCode {
+        OK,
+        M_FORBIDDEN,
+        M_USER_DEACTIVATED
+    }
+
     public class LoginHandler
     {
          public ResponseData LoginResponse {get; private set;}
 
          public LoginHandler(string requestStr, HttpListenerResponse response) {
              dynamic req = JsonConvert.DeserializeObject(requestStr);
-             if(req.user == null){
-                 LoginResponse = new ResponseData(response, "400 Bad request", "application/json", 400);
-             } else {
-                 LoginResponse = new ResponseData(response, JsonConvert.SerializeObject(new JSON_Login_Response("@" + req.user + ":dev.dunkelmann.eu", "sdsdasdsds", "dev.dunkelmann.eu", "ABCDEFGHI")), "application/json", 200);
+
+             try {
+                 string device_id;
+                 if(req.device_id != null){
+                     device_id = req.device_id;
+                 } else {
+                     device_id = generateDeviceID(Program.RANDOM);
+                 }
+
+                 if(validate_login() == ELoginErrorCode.OK){
+                     LoginResponse = new ResponseData(response, JsonConvert.SerializeObject(new JSON_Login_Response("@" + req.identifier.user + ":" + Program.HOMESERVER_NAME, "sdsdasdsds", Program.HOMESERVER_NAME, device_id)), "application/json", 200);
+                 } else {
+                     LoginResponse = new ResponseData(response, "{\"errcode\":\"" + validate_login().ToString() + "\"}", "application/json", 403);
+                 }
+             } catch (Exception ex) {
+                 LoginResponse = new ResponseData(response, "{\"errcode\":\"M_UNKNOWN\", \"error\":\"" + ex.Message + "\"}", "application/json", 400);
              }
+         }
+
+         private string generateAccessToken(){
+             return "not_implemented";
+         }
+
+         private string generateDeviceID(Random rnd){
+             string char_store = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+             string device_id = "";
+             int length = rnd.Next(5, 10+1); //5-10
+             for(int i = 0; i<length; i++){
+                 int index = rnd.Next(0, char_store.Length); //0-25
+                 device_id += char_store[index];
+             }
+             return device_id;
+         }
+
+         private ELoginErrorCode validate_login() {
+             return ELoginErrorCode.M_FORBIDDEN;
          }
     }
 }
--- Insomnium/src/Insomnium/Program.cs
+++ Insomnium/src/Insomnium/Program.cs
@@ -8,6 +8,10 @@ namespace Insomnium
 
         public static string[] SUPPORTED_LOGIN_FLOWS = {"m.login.password"};
         public static string VERSION = "1.0.0-alpha1";
+
+        public static string HOMESERVER_NAME = "dev.dunkelmann.eu";
+
+        public static Random RANDOM = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
